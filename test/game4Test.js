a8c65636566817ef49db4ad4cb71169b27a2aae3
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-02-07 20:01:15 +0000

    Created nested mappings for game4Test
    // Hardhat will create 10 accounts for you by default
    		// you can get one of this accounts with ethers.provider.getSigner
    		// and passing in the zero-based indexed of the signer you want:

--- test/game4Test.js
+++ test/game4Test.js
@@ -1,21 +1,31 @@
+/** @format */
+
 const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 const { assert } = require('chai');
 
 describe('Game4', function () {
-  async function deployContractAndSetVariables() {
-    const Game = await ethers.getContractFactory('Game4');
-    const game = await Game.deploy();
+	async function deployContractAndSetVariables() {
+		const Game = await ethers.getContractFactory('Game4');
+		const game = await Game.deploy();
 
-    return { game };
-  }
-  it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+		// Hardhat will create 10 accounts for you by default
+		// you can get one of this accounts with ethers.provider.getSigner
+		// and passing in the zero-based indexed of the signer you want:
 
-    // nested mappings are rough :}
+		const signer = ethers.provider.getSigner(0);
+		const address = await signer.getAddress();
 
-    await game.win();
+		return { game, address };
+	}
+	it('should be a winner', async function () {
+		const { game, address } = await loadFixture(deployContractAndSetVariables);
 
-    // leave this assertion as-is
-    assert(await game.isWon(), 'You did not win the game');
-  });
+		// nested mappings are rough :}
+		await game.write(address);
+
+		await game.win(address);
+
+		// leave this assertion as-is
+		assert(await game.isWon(), 'You did not win the game');
+	});
 });
