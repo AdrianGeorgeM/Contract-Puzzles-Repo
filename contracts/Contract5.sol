b0bbfa38e7be3787a7e425c2c8b1b2e4c80b804f
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-02-07 20:16:27 +0000

    Debugging with Hardhat Network
    Hardhat comes built-in with Hardhat Network, a local Ethereum network designed for development. It allows you to deploy your contracts, run your tests and debug your code, all within the confines of your local machine. It's the default network Hardhat that connects to, so you don't need to set up anything for it to work. Just run your tests.

--- contracts/Contract5.sol
+++ contracts/Contract5.sol
@@ -1,14 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
+import "hardhat/console.sol";
 
+// When running your contracts and tests on Hardhat Network you can print logging messages and contract variables calling console.log() from your Solidity code.
 contract Game5 {
-  bool public isWon;
+    bool public isWon;
 
-  address threshold = 0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf;
+    address threshold = 0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf;
 
-  function win() external {
-    require(bytes20(msg.sender) < bytes20(threshold), "Nope. Try again!");
+    function win() external {
+        require(bytes20(msg.sender) < bytes20(threshold), "Nope. Try again!");
 
-    isWon = true;
-  }
+        console.log("msg.sender: ", msg.sender);
+        isWon = true;
+    }
 }
